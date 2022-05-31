ca25aae85e206132a40810e6b79e47b7f8b77852
stalwartBunny <constructingintelligence@gmail.com>
2022-05-31 12:30:14 -0500

    got the board to develop

--- src/Chess/ChessMain.py
+++ src/Chess/ChessMain.py
@@ -12,7 +12,7 @@ IMAGES = {}
 def loadImages():
     pieces = ["wp", "bp", "wR", "bR", "wN", "bN", "wB", "bB", "wQ", "bQ", "wK", "bK"]
     for piece in pieces:
-        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png") (SQ_SIZE, SQ_SIZE))
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
     #is accessible from IMAGES["*"]
 
 
@@ -25,6 +25,35 @@ def main():
     gs = ChessEngine.GameState()
     print(gs.board)
     loadImages()
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
 
+#if __name__ == "__main__":
+#    main()
+
+#handles all graphic states
+def drawGameState(screen, gs):
+    drawBoard(screen)
+    drawPieces(screen, gs.board)
+
+def drawBoard(screen):
+        colors = [p.Color('white'), p.Color('gray')]
+        for r in range(DIMENSION):
+            for c in range(DIMENSION):
+                color = colors[((r+c)%2)]
+                p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 main()
--- src/Chess/modules/ChessEngine.py
+++ src/Chess/modules/ChessEngine.py
@@ -4,14 +4,15 @@
 class GameState():
 #alter this boardstate tracker to use numpi arrays for more efficient AI??
     def __init__(self):
-        self.board = {
-            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"]
-            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"]
-            ["--", "--", "--", "--", "--", "--", "--", "--"]
-            ["--", "--", "--", "--", "--", "--", "--", "--"]
-            ["--", "--", "--", "--", "--", "--", "--", "--"]
-            ["--", "--", "--", "--", "--", "--", "--", "--"]
-            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"]
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
-    self.whiteToMove = True
-    self.moveLog = []
+        ]
+        self.whiteToMove = True
+        self.moveLog = []
